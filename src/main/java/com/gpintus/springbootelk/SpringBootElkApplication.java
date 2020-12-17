c155a1bfee84bf1714f513c50b97ed852823acb5
Gavino Pintus <gavino.pintus@gmail.com>
2020-12-17 02:41:57 +0100

    Add log just for demo purposes

--- src/main/java/com/gpintus/springbootelk/SpringBootElkApplication.java
+++ src/main/java/com/gpintus/springbootelk/SpringBootElkApplication.java
@@ -1,13 +1,17 @@
 package com.gpintus.springbootelk;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringBootElkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootElkApplication.class, args);
+
+		log.info("The application spring-boot-elk is started");
 	}
 
 }
